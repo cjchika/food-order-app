d375e5080dca5c85d38cb41e9bbc6a90d659b22f
Chijioke <8pluscreative@gmail.com>
2022-01-11 02:19:54 -0800

    updating...

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,25 @@
+import { Fragment } from 'react';
+import { ReactDOM } from 'react-dom';
+
 import styles from './Modal.module.css'
 
+const Backdrop = props => {
+	return <div className={styles.backdrop}></div>
+};
+
+const ModalOverlay = props => {
+	return (
+	<div className={styles.modal}>
+	<div className={styles.content}>{props.children}</div>
+	</div>
+	);
+};
+
 const Modal = props => {
-    return 
+  return <Fragment>
+		{ReactDOM.createPortal(<Backdrop />)}
+		{ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>)}
+	</Fragment>
 };
 
 export default Modal;
