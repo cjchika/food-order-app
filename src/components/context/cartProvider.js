4c91ab0a2b74363b6df4b3cdb800adb80c7bb570
Chijioke <8pluscreative@gmail.com>
2022-01-11 18:48:01 -0800

    updating

--- src/components/context/cartProvider.js
+++ src/components/context/cartProvider.js
@@ -3,15 +3,17 @@ import CartContext from './cart-context';
 
 const CartProvider = props => {
 	const addItemToCartHandler = item => {};
-	
+
+	const removeItemFromCartHandler = id => {};
+
 	const CartContext = {
 		items: [],
 		totalAmount: 0,
-		addItem: (item) = {},
-		remoteItem: (id) => {}
+		addItem: addItemToCartHandler,
+		remoteItem: removeItemFromCartHandler
 	}
 
-	return <CartContext.Provider>
+	return <CartContext.Provider value={CartContext}>
 			{props.children}
 	</CartContext.Provider>
 };
