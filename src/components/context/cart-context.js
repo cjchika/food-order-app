9c684c6e61d728b045625fc523b609f3a878ca00
Chijioke <8pluscreative@gmail.com>
2022-01-11 19:06:48 -0800

    updating

--- src/components/context/cart-context.js
+++ src/components/context/cart-context.js
@@ -5,7 +5,6 @@ const CartContext = React.createContext({
     totalAmount: 0,
     addItem: (item) => {},
     remoteItem: (id) => {}
-
 });
 
 export default CartContext;
--- src/components/context/cartProvider.js
+++ src/components/context/cartProvider.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+	items: [], 
+	totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+	return defaultCartState;
+};
+
 const CartProvider = props => {
+	const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
 	const addItemToCartHandler = item => {};
 
 	const removeItemFromCartHandler = id => {};
 
-	const CartContext = {
+	const cartContext = {
 		items: [],
 		totalAmount: 0,
 		addItem: addItemToCartHandler,
 		remoteItem: removeItemFromCartHandler
 	}
 
-	return <CartContext.Provider value={CartContext}>
+	return (
+		<CartContext.Provider value={cartContext}>
 			{props.children}
-	</CartContext.Provider>
+		</CartContext.Provider>
+	);
 };
 
 export default CartProvider;
