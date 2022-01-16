312cf124ce3d27f0c6a27f11779a31b61123acfc
Chijioke <8pluscreative@gmail.com>
2022-01-16 03:59:30 -0800

    Updating checkout page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
 import CartContext from "../context/cart-context";
+import Checkout from './Checkout'
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -18,6 +20,10 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount: 1});
   };
+
+  const orderHandler = () => {
+    setIsCheckout(true)
+  }
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
@@ -33,6 +39,13 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+
+  const modalAction = <div className={styles.actions}>
+  <button className={styles["button--alt"]} onClick={props.onClose}>
+    Close
+  </button>{hasItems && <button className={styles.button} onClick={orderHandler}>Order</button>}
+  </div>
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -40,12 +53,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={styles.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout />}
+      {!isCheckout && modalAction}
     </Modal>
   );
 };
