0ec546f9ab2f6e9c32907fbfa9e157d2fdfb8432
Chijioke Chika <8pluscreative@gmail.com>
2022-06-01 00:36:41 -0400

    Update

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -9,7 +9,7 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === "ADD") {
-		const updatedTotalAmount = state.amount + action.item.price * action.item.amount;
+		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
 		const existingCartItemIndex = state.items.findIndex(
 			(item) => item.id === action.item.id
@@ -33,6 +33,26 @@ const cartReducer = (state, action) => {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount
 		};
+	}
+	if (action.type === "REMOVE") {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter(item => item.id !== action.id); 
+		} else {
+			const updatedItem = {... existingItem, amount: existingItem.amount - 1};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount
+		}
 	}
 	return defaultCartState;
 };
