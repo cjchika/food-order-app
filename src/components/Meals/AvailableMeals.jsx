bf3f8861fec260f89b91c0de63ae74f0ec1b8208
Chijioke Chika <8pluscreative@gmail.com>
2022-06-04 01:46:44 -0400

    Fetching Data from Firebase DB

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,42 @@
+import { useState, useEffect, useCallback } from "react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import styles from "./AvailableMeals.module.css";
 
-const SUB_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealList = SUB_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+
+  const fetchMealsHandler = useCallback(async () => {
+    setIsLoading(true)
+    setError(null)
+    
+    try {
+      const response = await fetch('https://food-order-app-420a4-default-rtdb.firebaseio.com/meals.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+  
+      const data = await response.json(meals);
+  
+      console.log(data);
+  
+      setMeals(data);
+      setIsLoading(false)
+    } catch(error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }, []);
+  
+  useEffect(() => {
+    fetchMealsHandler();
+  }, [fetchMealsHandler])
+  const mealList = meals.map((meal) => (
     <MealItem
 			id={meal.id}
       key={meal.id}
